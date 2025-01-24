5912d3affd349a60994bd9649da43cba9880d345
Abhay Mall <136182755+AbhayMall@users.noreply.github.com>
2025-01-24 20:21:51 +0530

    codesphere

---
+++ models/user.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+// yaha humna mongoose library import kiya iska vajah se schema define karta hai or mongodb databse ka sath jorta hai
+
+var Schema = mongoose.Schema;
+
+//schema kya hota hai?
+//Schema ek class hota hai jo humara model define karta hai
+//is schema ke madhyam se hum apne collection ka schema define karte hai
+//jaise ki collection name, field name, field type, field validation etc
+
+
+userSchema = new Schema( {
+
+	
+	unique_id: Number,
+	email: String,
+	username: String,
+	password: String,
+	passwordConf: String
+}),
+//jasa yaha humna ek blueprint tyaar kiya hai isma id , email etc liya hai
+//orr sab string data type ka hai
+//or passwordConf field ko bhi define kiya hai jo password field ki confirmation hai
+
+User = mongoose.model('User', userSchema);
+//yaha humne ek model define kiya hai jo humara collection ka schema hai
+//is model ke madhyam se hum apne collection ka data insert, update, delete etc
+//karte hai
+
+
+module.exports = User;
+//yaha humne ek module export kiya hai jo humara User model hai
+//is module ko apne file mein import karke hum apne collection ka data access kar s
+//kete hai
---
+++ routes/index.js
@@ -0,0 +1,178 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+
+//express routing and middleware framework provide karta hai
+//ye router use karte hai
+
+//router handle route
+
+//User import mongoose model taki voh user ke sath interact kar sake
+
+router.get('/', function (req, res, next) {
+	return res.render('about.ejs');
+});
+
+//// when a user visits the homepage (/), the server responds by sending the rendered HTML of the about.ejs file
+
+
+router.get('/index', function (req, res, next) {
+	return res.render('index.ejs');
+});
+
+// when a user visits the homepage (/index), the server responds by sending the rendered HTML of the index.ejs file
+
+
+//HANDLES USER REGISTRATION
+
+router.post('/', function(req, res, next) {
+	console.log(req.body);
+	var personInfo = req.body;
+
+
+	if(!personInfo.email || !personInfo.username || !personInfo.password || !personInfo.passwordConf){
+		res.send();
+	} else {
+		if (personInfo.password == personInfo.passwordConf) {
+
+			User.findOne({email:personInfo.email},function(err,data){
+				if(!data){
+					var c;
+					User.findOne({},function(err,data){
+
+						if (data) {
+							console.log("if");
+							c = data.unique_id + 1;
+						}else{
+							c=1;
+						}
+
+						var newPerson = new User({
+							unique_id:c,
+							email:personInfo.email,
+							username: personInfo.username,
+							password: personInfo.password,
+							passwordConf: personInfo.passwordConf
+						});
+
+						newPerson.save(function(err, Person){
+							if(err)
+								console.log(err);
+							else
+								console.log('Success');
+						});
+
+					}).sort({_id: -1}).limit(1);
+					res.send({"Success":"You are regestered,You can login now."});
+				}else{
+					res.send({"Success":"Email is already used."});
+				}
+
+			});
+		}else{
+			res.send({"Success":"password is not matched"});
+		}
+	}
+});
+
+//login page pe jana ho tho yeh route use karega
+router.get('/login', function (req, res, next) {
+	return res.render('login.ejs');
+});
+//data page pe jana ho tho yeh route use karega
+router.get('/data', function (req, res, next) {
+	return res.render('data.ejs');
+});
+//login page pe  ho tho yeh route use karega
+//isma apan phele check karenge ki user exist karta hai ya nahi
+//agar exist karta hai toh uska password check karenge
+//agar password match karta hai toh user ko login kar denge
+
+router.post('/login', function (req, res, next) {
+	//console.log(req.body);
+	User.findOne({email:req.body.email},function(err,data){
+		if(data){
+			
+			if(data.password==req.body.password){
+				//console.log("Done Login");
+				req.session.userId = data.unique_id;
+				//console.log(req.session.userId);
+				res.send({"Success":"Success!"});
+				
+			}else{
+				res.send({"Success":"Wrong password!"});
+			}
+		}else{
+			res.send({"Success":"This Email Is not regestered!"});
+		}
+	});
+});
+//profile page pe jana ho tho yeh route use karega
+//isme apan check karenge ki user login hai ya nahi
+//agar login hai toh uska data profile page pe dikhayenge
+//agar login nahi hai toh use login page pe redirect karenge
+//
+router.get('/profile', function (req, res, next) {
+	console.log("profile");
+	User.findOne({unique_id:req.session.userId},function(err,data){
+		console.log("data");
+		console.log(data);
+		if(!data){
+			res.redirect('/');
+		}else{
+			//console.log("found");
+			return res.render('data.ejs', {"name":data.username,"email":data.email});
+		}
+	});
+});
+//logout page pe jana ho tho yeh route use karega
+
+router.get('/logout', function (req, res, next) {
+	console.log("logout")
+	if (req.session) {
+    // delete session object
+    req.session.destroy(function (err) {
+    	if (err) {
+    		return next(err);
+    	} else {
+    		return res.redirect('/');
+    	}
+    });
+}
+});
+
+router.get('/forgetpass', function (req, res, next) {
+	res.render("forget.ejs");
+});
+
+// yaha se forget password hai
+
+router.post('/forgetpass', function (req, res, next) {
+	//console.log('req.body');
+	//console.log(req.body);
+	User.findOne({email:req.body.email},function(err,data){
+		console.log(data);
+		if(!data){
+			res.send({"Success":"This Email Is not regestered!"});
+		}else{
+			// res.send({"Success":"Success!"});
+			if (req.body.password==req.body.passwordConf) {
+			data.password=req.body.password;
+			data.passwordConf=req.body.passwordConf;
+
+			data.save(function(err, Person){
+				if(err)
+					console.log(err);
+				else
+					console.log('Success');
+					res.send({"Success":"Password changed!"});
+			});
+		}else{
+			res.send({"Success":"Password does not matched! Both Password should be same."});
+		}
+		}
+	});
+	
+});
+
+module.exports = router;
